b354677a5ced68e6ded43f10a99bfc0837ea432f
Isaac Murchie <imurchie@berkeley.edu>
2013-04-03 14:31:25 -0700

    Template for cakrabandha
    First commit.

---
+++ cakra.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# template for find possible instances of the cakra ('wheel') formation in Sanskrit literature
+# very (very very) basic at this point... single type.
+
