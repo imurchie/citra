476d22217128c2d30f2af1f67670534cc3c90e96
Isaac Murchie <imurchie@berkeley.edu>
2013-04-08 10:38:48 -0700

    Added test for lotus figure

---
+++ Padma.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+
+# implements the test for the padma ('lotus') figure
+
+from SyllableTools import parseSyllables
+from VerseTools import Verse
+
+
+
+# the most basic requirement of the padma figure is that
+# the first and last syllables of all the quarters be the same
+def isPadma(verse):
+	#pada_ab = parseSyllables(verse[1])
+	#pada_cd = parseSyllables(verse[2])
+	
+	#half_length = len(pada_ab)
+	#quarter_length = half_length / 2
+	
+	pada_a = verse.getPada(Verse.A) #parseSyllables(verse.getPada(Verse.A))#pada_ab[0:quarter_length]
+	pada_b = verse.getPada(Verse.B) #parseSyllables(verse.getPada(Verse.B))#pada_ab[quarter_length:half_length]
+	pada_c = verse.getPada(Verse.C) #parseSyllables(verse.getPada(Verse.C))#pada_cd[0:quarter_length]
+	pada_d = verse.getPada(Verse.D) #parseSyllables(verse.getPada(Verse.D))#pada_cd[quarter_length:half_length]
+	
+	if pada_a[0] == pada_a[-1]\
+		and  pada_a[-1] == pada_b[0]\
+		and pada_b[0] == pada_b[-1]\
+		and pada_b[-1] == pada_c[0]\
+		and pada_c[0] == pada_c[-1]\
+		and pada_c[-1] == pada_d[0]\
+		and pada_d[0] == pada_d[-1]:
+		return True
+	
+	return False
