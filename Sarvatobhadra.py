274ac7f5f0d90c465d439be7e7b125e81a8d2ab8
Isaac Murchie <imurchie@berkeley.edu>
2013-04-10 09:00:54 -0700

    Finished

--- Sarvatobhadra.py
+++ Sarvatobhadra.py
@@ -19,27 +19,46 @@ The test: Each line must be a palindrome
 
 
 def isPalindrome(line):
-	palindrome = True
 	i = 0
 	j = len(line) - 1
-	
 	while i < j:
-		print line[i], '...', line[j]
 		if line[i] != line[j]:
-			palindrome = False
-			break
-		
+			return False
 		i += 1
 		j -= 1
 	
-	return palindrome
+	return True
 
 
 # expects a Verse object
 def isSarvatobhadra(verse):
+	matrix = []
 	for line in verse:
-		palindrome = isPalindrome(line)
-		print palindrome
-	
-	return False
+		if not isPalindrome(line):
+			# if any line is not a palindrome, the whole thing is False
+			return False
+		
+		# no point in doing anything with the matrix until here
+		matrix.append(line)
+		
+	# at this point the lines are all palindromes in their own right
+	# and the matrix is half full
+	# but needs to have the verse in reverse 
+	# i.e., we want to end up with [[a], [b], [c], [d], [d], [c], [b], [a]]
+	for line in reversed(matrix):
+		matrix.append(line)
+		
+	# now we need to find the 'vertical' palindrome
+	# go through the verse and compare to matrix
+	i = 0
+	j = 0
+	while i < len(matrix):
+		while j < len(matrix):
+			print matrix[i][j], '...', matrix[j][i]
+			if matrix[i][j] != matrix[j][i]:
+				return False
+			j += 1
+		i += 1
+		
+	return True
 	
